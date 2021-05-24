43c7b61e6e0c8c9aefb074b29993ddb402a1073c
brunocosta1 <brunocosta.cg1998@gmail.com>
2021-05-24 11:04:09 -0300

    Adicionando algumas coisas para instalação do alacritty

--- instalation.sh
+++ instalation.sh
@@ -9,9 +9,11 @@ sudo apt update
 # Installing the core programs with apt
 
 sudo apt install fonts-powerline i3 i3blocks fzf fonts-firacode rofi vim git build-essential tmux zsh cmake python3 python3-dev
-sudo apt install kitty
 
-# Installing other programs 
+# Dependencies for alacritty
 
+sudo apt install pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev 
+sudo apt install cargo
+cargo install alacritty
 
 
