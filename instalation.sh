a5f8fb1c90f142cfbcf002753357d2fd9c39f0f1
brunocosta1 <brunocosta.cg1998@gmail.com>
2021-05-24 01:01:08 -0300

    Alterando o terminal padrão para o kitty

--- instalation.sh
+++ instalation.sh
@@ -9,6 +9,7 @@ sudo apt update
 # Installing the core programs with apt
 
 sudo apt install fonts-powerline i3 i3blocks fzf fonts-firacode rofi vim git build-essential tmux zsh cmake python3 python3-dev
+sudo apt install kitty
 
 # Installing other programs 
 
