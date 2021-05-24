a2b82ba51436ea3bfbeba987e998ee9e3d34a172
brunocosta1 <brunocosta.cg1998@gmail.com>
2021-05-24 19:33:53 -0300

    Adicionando alguns programas para serem baixados.

--- instalation.sh
+++ instalation.sh
@@ -8,7 +8,7 @@ sudo apt update
 
 # Installing the core programs with apt
 
-sudo apt install fonts-powerline i3 i3blocks fzf fonts-firacode rofi neovim vim git build-essential tmux zsh cmake python3 python3-dev xsel
+sudo apt install fonts-powerline i3 i3blocks fzf fonts-firacode rofi neovim vim git build-essential tmux zsh cmake python3 python3-dev xsel feh compton compton-dconf
 
 # Dependencies for alacritty
 
