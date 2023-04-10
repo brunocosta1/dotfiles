4287108c98d7c01c48f9973194ce75d1cfe0adb3
brunocosta1 <brunocosta.cg1998@gmail.com>
2023-04-09 22:27:31 -0300

    Ajustando arquivos

--- lua/servers.lua
+++ lua/servers.lua
@@ -7,34 +7,36 @@ vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<C
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-        -- Mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-        vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl',
+    '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
-local servers = {'tailwindcss', 'bashls', 'rust_analyzer', 'lua_ls', 'pyright', 'clangd', 'vimls', 'tsserver', 'cssls', 'html', 'solargraph', 'intelephense', 'jsonls', 'sqls', 'perlnavigator' }
+local servers = { 'tailwindcss', 'bashls', 'rust_analyzer', 'lua_ls', 'pyright', 'clangd', 'vimls', 'tsserver', 'cssls',
+  'html', 'solargraph', 'intelephense', 'jsonls', 'sqlls', 'perlnavigator' }
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = servers
+  ensure_installed = servers
 })
 
-require('lspconfig').jdtls.setup{}
+require('lspconfig').jdtls.setup {}
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 
@@ -55,17 +57,17 @@ require('lspconfig').jdtls.setup{}
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 for _, name in pairs(servers) do
-        require('lspconfig')[name].setup {
-                on_attach = on_attach,
-                flags = {
-                        -- This will be the default in neovim 0.7+
-                        debounce_text_changes = 150,
-                }
-        }
+  require('lspconfig')[name].setup {
+    on_attach = on_attach,
+    flags = {
+      -- This will be the default in neovim 0.7+
+      debounce_text_changes = 150,
+    }
+  }
 
-        require('lspconfig')[name].setup {
-                capabilities = capabilities
-        }
+  require('lspconfig')[name].setup {
+    capabilities = capabilities
+  }
 
 end
 --
@@ -73,13 +75,13 @@ end
 
 local system_name
 if vim.fn.has("mac") == 1 then
-        system_name = "macOS"
+  system_name = "macOS"
 elseif vim.fn.has("unix") == 1 then
-        system_name = "Linux"
+  system_name = "Linux"
 elseif vim.fn.has('win32') == 1 then
-        system_name = "Windows"
+  system_name = "Windows"
 else
-        print("Unsupported system for sumneko")
+  print("Unsupported system for sumneko")
 end
 
 -- set the path to the sumneko installation; if you previously installed via the now deprecated :LspInstall, use
@@ -116,9 +118,7 @@ table.insert(runtime_path, "lua/?/init.lua")
 --                 },
 --         },
 -- }
+
+
 require('Comment').setup()
-require('lspconfig').sqls.setup {
-        on_attach = function(client, bufnr)
-                require('sqls').on_attach(client, bufnr)
-        end
-}
+--require('lspconfig').sqls.setup {}
