4f6b18bb9a182790fdf58a79c5901e4e139c2291
Trabalho <trabalho@pop-os.localdomain>
2026-04-09 11:55:16 -0300

    removendo arquivos

--- instalacao_nvim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-sudo apt install -y neovim git ripgrep fd
-
-git clone https://github.com/brunocosta1/dotfiles.git ~/.dotfiles
-
-ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
